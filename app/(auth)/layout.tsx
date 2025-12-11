51d8559cdd563fe5029b8e3012533121356d6681
Joxeph <codewithjoxeph@gmail.com>
2025-12-11 15:00:10 +0100

    test

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,8 +4,8 @@ import type { PropsWithChildren } from "react"
 
 export default function AuthLayout({ children }: PropsWithChildren) {
     return <section className="w-full h-full py-20 px-4 col-center gap-8 min-h-screen relative">
-        <Image src={"/pattern.png"} className="-z-1 select-none pointer-events-none" aria-hidden fill alt="" />
-        <Logo />
+        {/* <Image src={"/pattern.png"} className="-z-1 select-none pointer-events-none" aria-hidden fill alt="" /> */}
+        {/* <Logo /> */}
         <main className="py-10 px-6 md:px-10 z-1  max-w-[448px] bg-white w-full rounded-2xl ring ring-grey">
 
             {children}
