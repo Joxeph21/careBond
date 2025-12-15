26ffe1fe9231a8a9f65bf86bfc13e04f018d0f60
Joxeph <codewithjoxeph@gmail.com>
2025-12-15 12:39:35 +0100

    feat: Proxy added to handle prequest and prevent unauthorized access to dashboard

---
+++ proxy.ts
@@ -0,0 +1,36 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { CONFIG } from "./utils/config";
+
+const PUBLIC_ROUTES = [
+  "/login",
+  "/forgot-password",
+  "/create-password",
+  "/2fa",
+  "/verify",
+];
+
+export default async function proxy(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  const serverCookies = await cookies();
+
+  const isPublicRoute = PUBLIC_ROUTES.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  const refresh = serverCookies.get(CONFIG.REFRESH_TOKEN_IDENTIFIER)?.value;
+
+  if (!refresh && !isPublicRoute) {
+    console.log("Middleware: ⛔No refresh token found, redirecting to login");
+    return NextResponse.redirect(new URL("/login", req.nextUrl.origin));
+  }
+
+
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
