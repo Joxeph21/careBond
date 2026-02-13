ac8c4b9d694b0c2cf1df4a275f66270fb31d7940
Joxeph21 <codewithjoxeph@gmail.com>
2026-02-13 22:16:40 +0100

    feat: add Utendo local font configuration.

--- app/fonts.ts
+++ app/fonts.ts
@@ -12,10 +12,11 @@ export const Utendo = localFont({
     { path: "./fonts/Utendo-Regular.ttf", weight: "400", style: "normal" },
     { path: "./fonts/Utendo-Medium.ttf", weight: "500", style: "normal" },
     {
-      path: "./fonts/Utendo-SemiBold.ttf",
+      path: "./fonts/Utendo-Semibold.ttf",
       weight: "600",
       style: "normal",
     },
+    { path: "./fonts/Utendo-Bold.ttf", weight: "700", style: "normal" },
     {
       path: "./fonts/Utendo-ExtraBold.ttf",
       weight: "800",
